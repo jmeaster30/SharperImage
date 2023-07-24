e50f921b2fe8eff547427976d8033fedaeb4e924
jmeaster30 <jmeasterday@comcast.net>
2023-07-23 20:34:09 -0400

    Added a couple more png chunks

--- SharperImage/Formats/PngImage.cs
+++ SharperImage/Formats/PngImage.cs
@@ -32,23 +32,13 @@ public class PngImage : IImage
         if (!signature.SequenceEqual(new byte[] { 137, 80, 78, 71, 13, 10, 26, 10 }))
             throw new ImageDecodeException("Unexpected PNG file signature");
 
-        ImageHeader? imageHeader = null;
         var chunks = new List<Chunk>();
         while (stream.Position != stream.Length)
         {
-            var chunk = Chunk.Load(stream);
-            switch (chunk.Type)
-            {
-                case "IHDR":
-                    imageHeader = chunk.ToImageHeader();
-                    break;
-                default: 
-                    Console.WriteLine($"Chunk {chunk.Type}");
-                    break;
-                    //throw new ArgumentException("Unexpected chunk type", nameof(chunk));
-            }
-            chunks.Add(chunk);
+            chunks.Add(Chunk.Load(stream));
         }
+        
+        var imageHeader = (ImageHeader?)chunks.SingleOrDefault(x => x.Is<ImageHeader>());
 
         if (imageHeader == null)
         {
@@ -74,7 +64,16 @@ public class PngImage : IImage
         {
             throw new ImageDecodeException("Unexpected compression method :(");
         }
-        
+
+        switch (imageHeader.ColorType)
+        {
+            case 3 when !chunks.Any(x => x.Is<Palette>()):
+                throw new ImageDecodeException("Expected a palette chunk with the color type 3");
+            case 0 or 4 when chunks.Any(x => x.Is<Palette>()):
+                throw new ImageDecodeException(
+                    "Expected no palette chunks is the color type 0 or 4 but found a palette chunk");
+        }
+
         _width = imageHeader.Width;
         _height = imageHeader.Height;
 
@@ -82,41 +81,59 @@ public class PngImage : IImage
         Console.WriteLine(JsonConvert.SerializeObject(chunks));
     }
     
-    private class Chunk
+    private abstract class Chunk
     {
         public uint Length { get; set; }
         public string Type { get; set; } = "";
         public byte[] Data { get; set; } = { };
         public uint Crc { get; set; }
 
+        public abstract bool Is<T>();
+
         public static Chunk Load(Stream stream)
         {
             var chunkLength = stream.ReadU32();
-            return new Chunk
+            var type = Encoding.UTF8.GetString(stream.ReadBytes(4));
+            Console.WriteLine(type);
+            var data = stream.ReadBytes(chunkLength);
+            var crc = stream.ReadU32();
+            return type switch
             {
-                Length = chunkLength,
-                Type = Encoding.UTF8.GetString(stream.ReadBytes(4)),
-                Data = stream.ReadBytes(chunkLength),
-                Crc = stream.ReadU32(),
-            };
-        }
-
-        public ImageHeader ToImageHeader()
-        {
-            return new ImageHeader
-            {
-                Width = Data[..4].ToU32(),
-                Height = Data[4..8].ToU32(),
-                BitDepth = Data[8],
-                ColorType = Data[9],
-                CompressionMethod = Data[10],
-                FilterMethod = Data[11],
-                InterlaceMethod = Data[12],
+                "IHDR" => new ImageHeader
+                {
+                    Length = chunkLength,
+                    Type = type,
+                    Data = data,
+                    Crc = crc,
+                    Width = data[..4].ToU32(),
+                    Height = data[4..8].ToU32(),
+                    BitDepth = data[8],
+                    ColorType = data[9],
+                    CompressionMethod = data[10],
+                    FilterMethod = data[11],
+                    InterlaceMethod = data[12],
+                },
+                "PLTE" => Palette.Load(chunkLength, type, data, crc),
+                "gAMA" => new Gamma
+                {
+                    Length = chunkLength,
+                    Type = type,
+                    Data = data,
+                    Crc = crc,
+                    Value = data[..4].ToU32(),
+                },
+                _ => new Unknown
+                {
+                    Length = chunkLength,
+                    Type = type,
+                    Data = data,
+                    Crc = crc,
+                }
             };
         }
     }
 
-    private class ImageHeader
+    private class ImageHeader : Chunk
     {
         public uint Width { get; set; }
         public uint Height { get; set; }
@@ -125,8 +142,59 @@ public class PngImage : IImage
         public byte CompressionMethod { get; set; }
         public byte FilterMethod { get; set; }
         public byte InterlaceMethod { get; set; }
+
+        public override bool Is<T>()
+        {
+            return typeof(T) == typeof(ImageHeader);
+        }
     }
 
+    private class Palette : Chunk
+    {
+        public List<(byte, byte, byte)> PaletteTable { get; set; } = new();
+
+        public override bool Is<T>()
+        {
+            return typeof(T) == typeof(Palette);
+        }
+        
+        public static Palette Load(uint length, string type, byte[] data, uint crc)
+        {
+            if (length % 3 != 0)
+            {
+                throw new ImageDecodeException("Pallette table has length not divisible by 3");
+            }
+            
+            return new Palette
+            {
+                Length = length,
+                Type = type,
+                Data = data,
+                Crc = crc,
+                PaletteTable = data.Chunk(3).Select(group => (group[0], group[1], group[2])).ToList(),
+            };
+        }
+    }
+
+    private class Gamma : Chunk
+    {
+        // Scaled by 100,000
+        public uint Value { get; set; }
+
+        public override bool Is<T>()
+        {
+            return typeof(T) == typeof(Gamma);
+        }
+    }
+
+    private class Unknown : Chunk
+    {
+        public override bool Is<T>()
+        {
+            return typeof(T) == typeof(Unknown);
+        }
+    }
+    
     public void Encode(Stream stream)
     {
         throw new NotImplementedException();
